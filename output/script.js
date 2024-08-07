5bf97687ddf594fe0959b9367eaa6314dc205e0b
Raul Guevara <127911070+RaulGuevara555@users.noreply.github.com>
2024-08-07 12:28:50 -0600

    Add files via upload

---
+++ output/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("Hola soy JavaScript");
+var valor1;
+for (var i; i < 10; i++) {
+  console.log(i);
+}
+var numero1;
+var constante = 23;
+var nombre = "Raul Guevara";
+console.log("Hola " + nombre);
+function setup() {
+  createCanvas(400, 400);
+}
+function draw() {
+  background(220);
+}
